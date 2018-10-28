63957abe3ba3029a967733929e08eb1c29f0a96e
Vipin Kumar <vipkmar@amazon.com>
2018-10-28 15:54:38 +0530

    module5

---
+++ src/EdurekaHW/module5/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package EdurekaHW.module5;
+import java.util.ArrayList;
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        System.out.println("Number of Employee in this list are :"+names.size());
+        names.add("Vipin");
+        names.add("Varun");
+        names.add("Arun");
+        System.out.println("Number of Employee in this list are :"+names.size());
+        for(int index=0;index<names.size();index++) {
+            int kk = names.indexOf(index);
+            System.out.println("Employee with EmployeeId : " + kk + " is " + names.get(index));
+        }
+    }
+}
